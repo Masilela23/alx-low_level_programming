77e534fe61a535cfbc81b3b4e24d8320cefc5313
Masilela23 <melusi.mokone@gmail.com>
2023-10-05 12:44:58 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - The program must use the function puts to print instruction
+ * rerurn: 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
