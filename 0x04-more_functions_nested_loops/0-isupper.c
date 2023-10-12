cea15cf13a7e24dd8ef33230c84f162e49f7d66b
Masilela23 <melusi.mokone@gmail.com>
2023-10-12 11:02:06 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - check letter
+ * @x: the number to be checked
+ * Return: 1 for upper or 0 for any other
+ */
+
+int _supper(int x)
+{
+	if (x >= 65 && x<= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
