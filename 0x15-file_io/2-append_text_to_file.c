eca644b2fe10069eff45c4506eeade3ec8115920
Masilela23 <melusi.mokone@gmail.com>
2023-11-27 12:55:06 -0800

    0x15-file_io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * append_text_to_file - this progm appends text at the end of a file
  *
@@ -11,27 +13,32 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int file_m;
-	int lett;
-	int mas;
+	int sime;
+	int len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
 	file_m = open(filename, O_WRONLY | O_APPEND);
 
 	if (file_m == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content == NULL)
+		text_content = "";
+
+	while (text_content[len] != '\0')
+	{
+		len++;
+	}
+
+	sime = write(file_m, text_content, len);
+
+	if (sime == -1)
 		return (-1);
 
-	if (text_content)
-	{
-		for (lett = 0; text_content[lett]; lett++)
-			;
-
-		mas = write(file_m, text_content, lett);
-
-		if (mas == -1)
-			return (-1);
-	}
 	close(file_m);
 
 	return (1);
