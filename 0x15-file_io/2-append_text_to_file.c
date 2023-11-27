e32233cbdec75b4013d5fce059c4fc03f242264e
Masilela23 <melusi.mokone@gmail.com>
2023-11-27 12:42:06 -0800

    file_io

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,38 @@
+/**
+ * append_text_to_file - this progm appends text at the end of a file
+ *
+ * @filename: name.
+ *
+ * @text_content: added content
+ *
+ * @Return: 1 if file exists, -1 if it fails
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file_m;
+	int lett;
+	int mas;
+
+	if (!filename)
+		return (-1);
+
+	file_m = open(filename, O_WRONLY | O_APPEND);
+
+	if (file_m == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (lett = 0; text_content[lett]; lett++)
+			;
+
+		mas = write(file_m, text_content, lett);
+
+		if (mas == -1)
+			return (-1);
+	}
+	close(file_m);
+
+	return (1);
+}
