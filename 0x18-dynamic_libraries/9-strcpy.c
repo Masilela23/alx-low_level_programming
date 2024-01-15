96ce967eef7d5599db1de01dc77274e948dd5a90
Masilela23 <melusi.mokone@gmail.com>
2024-01-15 10:31:21 -0800

    libdynamic.so

--- 0x18-dynamic_libraries/9-strcpy.c
+++ 0x18-dynamic_libraries/9-strcpy.c
@@ -4,23 +4,17 @@
  * _strcpy - a function that must copy a str. pointed by src
  * @dest: value entered in func
  * @src: value entered in func
- * @n: value entered in func
  * Return: dest
  */
 
 char *_strcpy(char *dest, char *src)
 {
-	int m = 0;
-	int q = 0;
+	int i = -1;
 
-	while (*(src + m) != '\0')
-	{
-		m++;
-	}
-	for ( ; q < m ; q++)
-	{
-		dest[q] = src[q];
-	}
-	dest[q] = '\0';
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
+
 	return (dest);
 }
