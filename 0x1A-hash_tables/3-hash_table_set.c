086c216fd628069f708353c938984009e18bdb3c
Masilela23 <melusi.mokone@gmail.com>
2024-02-02 11:10:33 -0800

    Mandatory

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,86 +15,43 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int slo = 0;
+	hash_node_t *mel;
+	char *value_copy;
+	unsigned long int index, m;
 
-	hash_node_t *new_hash = NULL, *temp = NULL;
-
-	new_hash = malloc(sizeof(hash_node_t));
-
-	if (ht == NULL || !key || strlen(key) == 0 || value == NULL)
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
-	if (new_hash == NULL)
+	value_copy = strdup(value);
+	if (value_copy == NULL)
 		return (0);
-	temp = new_hash;
-	slo = key_index((const unsigned char *)key, ht->size);
 
-	if (ht->array[slo] != NULL)
+	index = key_index((const unsigned char *)key, ht->size);
+	for (m = index; ht->array[m]; m++)
 	{
-		new_hash = ht->array[slo];
-
-		while (new_hash != NULL)
+		if (strcmp(ht->array[i]->key, key) == 0)
 		{
-			if (strcmp(new_hash->key, key) == 0)
-			{
-				free(new_hash->value);
-				new_hash->value = strdup(value);
-				free(temp);
-				return (1);
-			}
-			new_hash = new_hash->next;
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
 		}
 	}
-	new_hash = hash_table_pair(key, value);
-	if (new_hash == NULL)
+
+	mel = malloc(sizeof(hash_node_t));
+	if (mel == NULL)
 	{
-		free(new_hash);
+		free(value_copy);
 		return (0);
 	}
-	new_hash->next = ht->array[slot];
-	ht->array[slot] = new_hash;
-	free(temp);
+	mel->key = strdup(key);
+	if (mel->key == NULL)
+	{
+		free(mel);
+		return (0);
+	}
+	mel->value = value_copy;
+	mel->next = ht->array[index];
+	ht->array[index] = mel;
+
 	return (1);
-}
-
-
-/**
- *  hash_table_pair - Allocate memory for entry
- *
- *  @key: Key to hashtable
- *
- *  @value: Value associated with key
- *
- *  Return: Entry of key/value
- */
-hash_node_t *hash_table_pair(const char *key, const char *value)
-{
-	hash_node_t *entry = NULL;
-
-	entry = malloc(sizeof(hash_node_t));
-
-	if (entry == NULL)
-	{
-		return (NULL);
-	}
-
-	entry->key = strdup(key);
-
-	if (entry->key == NULL)
-	{
-		free(entry);
-		return (NULL);
-	}
-
-	entry->value = strdup(value);
-
-	if (entry->value == NULL)
-	{
-		free(entry->key);
-		free(entry);
-		return (NULL);
-	}
-	entry->next = NULL;
-
-	return (entry);
 }
